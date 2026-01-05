f9a8a16637919fc1065e64960988f5e6d0c4aaaf
Gokul <gokul1000@gmail.com>
2026-01-05 13:30:14 +0530

    pushed the first file of band name generator

---
+++ practise.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator")
+city_nme=input("enter your city name ")
+pet_name=input("enter the name of your pet ")
+print("amazing! your band name will be," , city_nme +" " +pet_name )
